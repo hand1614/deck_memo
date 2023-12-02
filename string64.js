9353636fd030f3a5b91a15cb5d4c75a3d0809d46
6218u <furaigonn520@gmail.com>
2023-12-02 12:44:25 +0900

    .

--- string64.js
+++ string64.js
@@ -1,51 +1,54 @@
 
 "use strict";
 
-const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_" ;
-
-const log_init = ( new Array( 56 ) ).fill( 0 ) ;
-
-function compress4 ( str, char64 = charset, log0k_init = log_init, log8k_init = log_init ) {
-  let   ret   = "" ;
-  const log0k = [ ...log0k_init ] ;
-  const log8k = [ ...log8k_init ] ;
-  for( const v of str ) {
-    const n       = v.codePointAt() ;
-    const index0k = log0k.indexOf( n ) ;
-    const index8k = log8k.indexOf( n & 0x1fff00 ) ;
-    ret +=
-      index0k !== -1               ? char64[ index0k ]
-    : n >> 8  === log0k[ 0 ] >> 8  ? char64[ 0x38 | n & 3 ] + char64[ 0x3f & n >> 2 ]
-    : index8k !== -1               ? char64[ 0x3c | n & 3 ] + char64[ 0x3f & n >> 2 ] + char64[ index8k ]
-    : n >> 16 === log0k[ 0 ] >> 16 ? char64[ 0x3c | n & 3 ] + char64[ 0x3f & n >> 2 ] + char64[ 0x38 | n >> 8 & 3 ] + char64[ 0x3f & n >> 10 ]
-    :                                char64[ 0x3c | n & 3 ] + char64[ 0x3f & n >> 2 ] + char64[ 0x3c | n >> 8 & 3 ] + char64[ 0x3f & n >> 10 ] + char64[ n >> 16 ] ;
-    for( let i = ( index8k + 1 || log8k.length ) - 1 ; i > 0 ; i-- ) log8k[ i ] = log8k[ i - 1 ] ;
-    log8k[ 0 ] = n & 0x1fff00 ;
-    for( let i = ( index0k + 1 || log0k.length ) - 1 ; i > 0 ; i-- ) log0k[ i ] = log0k[ i - 1 ] ;
-    log0k[ 0 ] = n ;
+class String64 {
+  static #init( init_obj ) {
+    return {
+      charset: init_obj.charset ?? "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_",
+      log_all: init_obj.log_all ?? ( new Array( 56 ) ).fill( 0 ),
+      log_hi:  init_obj.log_hi  ?? ( new Array( 56 ) ).fill( 0 ),
+    } ;
   }
-  return ret ;
-}
 
-function index_to_charcode( target, index ) { return target.charset.indexOf( target.str[ index ] ) ; }
-
-function archive4 ( str, char64 = charset, log0k_init = log_init, log8k_init = log_init ) {
-  let   ret    = "" ;
-  const log0k  = [ ...log0k_init ] ;
-  const log8k  = [ ...log8k_init ] ;
-  const code64 = new Proxy( { charset: char64, str: str }, { get: index_to_charcode } ) ;
-  for( let i = 0 ; i < str.length ; ) {
-    const n =
-      code64[ i ]     & 0x38 ^ 0x38 ? log0k[ code64[ i++ ] ]
-    : code64[ i ]     & 0x3c ^ 0x3c ? code64[ i++ ] & 3 | code64[ i++ ] << 2 | log8k[ 0 ]
-    : code64[ i + 2 ] & 0x38 ^ 0x38 ? code64[ i++ ] & 3 | code64[ i++ ] << 2 | log8k[ code64[ i++ ] ]
-    : code64[ i + 2 ] & 0x3c ^ 0x3c ? code64[ i++ ] & 3 | code64[ i++ ] << 2 | ( code64[ i++ ] & 3 ) << 8 | code64[ i++ ] << 10 | log8k[ 0 ] & 0x1f0000
-    :                                 code64[ i++ ] & 3 | code64[ i++ ] << 2 | ( code64[ i++ ] & 3 ) << 8 | code64[ i++ ] << 10 | code64[ i++ ] << 16 ;
-    for( let j = ( log8k.indexOf( n & 0x1fff00 ) + 1 || log8k.length ) - 1 ; j > 0 ; j-- ) log8k[ j ] = log8k[ j - 1 ] ;
-    log8k[ 0 ] = n & 0x1fff00 ;
-    for( let j = ( log0k.indexOf( n ) + 1 || log0k.length ) - 1 ; j > 0 ; j-- ) log0k[ j ] = log0k[ j - 1 ] ;
-    log0k[ 0 ] = n ;
-    ret += String.fromCodePoint( n ) ;
+  static encode ( str, init_obj = {} ) {
+    let   ret = "" ;
+    const { charset, log_all, log_hi } = this.#init( init_obj ) ;
+    for( const v of str ) {
+      const n         = v.codePointAt() ;
+      const index_all = log_all.indexOf( n ) ;
+      const index_hi  = log_hi.indexOf( n & 0x1fff00 ) ;
+      ret +=
+        index_all !== -1                 ? charset[ index_all ]
+      : n >> 8    === log_all[ 0 ] >> 8  ? charset[ 0x38 | n & 3 ] + charset[ 0x3f & n >> 2 ]
+      : index_hi  !== -1                 ? charset[ 0x3c | n & 3 ] + charset[ 0x3f & n >> 2 ] + charset[ index_hi ]
+      : n >> 16   === log_all[ 0 ] >> 16 ? charset[ 0x3c | n & 3 ] + charset[ 0x3f & n >> 2 ] + charset[ 0x38 | n >> 8 & 3 ] + charset[ 0x3f & n >> 10 ]
+      :                                    charset[ 0x3c | n & 3 ] + charset[ 0x3f & n >> 2 ] + charset[ 0x3c | n >> 8 & 3 ] + charset[ 0x3f & n >> 10 ] + charset[ n >> 16 ] ;
+      for( let i = ( index_hi + 1 || log_hi.length ) - 1 ; i > 0 ; i-- ) log_hi[ i ] = log_hi[ i - 1 ] ;
+      log_hi[ 0 ] = n & 0x1fff00 ;
+      for( let i = ( index_all + 1 || log_all.length ) - 1 ; i > 0 ; i-- ) log_all[ i ] = log_all[ i - 1 ] ;
+      log_all[ 0 ] = n ;
+    }
+    return ret ;
   }
-  return ret ;
+
+  static decode ( str, init_obj = {} ) {
+    let   ret = "" ;
+    const { charset, log_all, log_hi } = this.#init( init_obj ) ;
+    const codelist = [] ;
+    for( const char of str ) codelist.push( charset.indexOf( char ) ) ;
+    for( let i = 0 ; i < str.length ; ) {
+      const n =
+        codelist[ i ]     & 0x38 ^ 0x38 ? log_all[ codelist[ i++ ] ]
+      : codelist[ i ]     & 0x3c ^ 0x3c ? codelist[ i++ ] & 3 | codelist[ i++ ] << 2 | log_hi[ 0 ]
+      : codelist[ i + 2 ] & 0x38 ^ 0x38 ? codelist[ i++ ] & 3 | codelist[ i++ ] << 2 | log_hi[ codelist[ i++ ] ]
+      : codelist[ i + 2 ] & 0x3c ^ 0x3c ? codelist[ i++ ] & 3 | codelist[ i++ ] << 2 | ( codelist[ i++ ] & 3 ) << 8 | codelist[ i++ ] << 10 | log_hi[ 0 ] & 0x1f0000
+      :                                   codelist[ i++ ] & 3 | codelist[ i++ ] << 2 | ( codelist[ i++ ] & 3 ) << 8 | codelist[ i++ ] << 10 | codelist[ i++ ] << 16 ;
+      for( let j = ( log_hi.indexOf( n & 0x1fff00 ) + 1 || log_hi.length ) - 1 ; j > 0 ; j-- ) log_hi[ j ] = log_hi[ j - 1 ] ;
+      log_hi[ 0 ] = n & 0x1fff00 ;
+      for( let j = ( log_all.indexOf( n ) + 1 || log_all.length ) - 1 ; j > 0 ; j-- ) log_all[ j ] = log_all[ j - 1 ] ;
+      log_all[ 0 ] = n ;
+      ret += String.fromCodePoint( n ) ;
+    }
+    return ret ;
+  }
 }
